14acb8f9a3ae440ce28ae9f5c6c600dfee986160
Tao Lin <tlinqu@gmail.com>
2014-02-01 16:12:02 -0500

    Write the test in alternative style

--- 1820OS_08_Code/01_button_directive/button.spec.js
+++ 1820OS_08_Code/01_button_directive/button.spec.js
@@ -1,10 +1,10 @@
 describe('button directive', function () {
     var $compile, $rootScope;
     beforeEach(module('directives.button'));
-    beforeEach(inject(function(_$compile_, _$rootScope_) {
+    beforeEach(function(){inject(function(_$compile_, _$rootScope_) {
         $compile = _$compile_;
-        $rootScope = _$rootScope_;
-    }));
+        $rootScope = _$rootScope_.$new();
+    })});
     it('adds a "btn" class to the button element', function() {
         var element = $compile('<button></button>')($rootScope);
         expect(element.hasClass('btn')).toBe(true);
