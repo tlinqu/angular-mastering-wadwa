2774ad8a8f3823a8381602c022967870441d57fd
tlin-fei <tao.lin@feisystems.com>
2014-02-14 14:14:02 -0500

    Add port number escaping notes for $resource

--- 1820OS_03_Code/07 - resource/resource.js
+++ 1820OS_03_Code/07 - resource/resource.js
@@ -1,5 +1,10 @@
 angular.module('resource', ['ngResource'])
   .factory('Users', function ($resource) {
+    // If there is port number in the url, e.g. http://localhost:8080
+    // Because of bug in AngularJS, see http://stackoverflow.com/questions/18758918/cors-with-angular-js-resource-and-servicestack-servies
+    // Port number need be escaped,in one of two ways:
+    // var Users = $resource('http://localhost\\:8080/users');
+    // var Users = $resource('http://localhost:8080\:8080/users');
     var Users = $resource('https://api.mongolab.com/api/1/databases/ascrum/collections/users/:id', {
       apiKey:'4fb51e55e4b02e56a67b0b66',
       id:'@_id.$oid'
