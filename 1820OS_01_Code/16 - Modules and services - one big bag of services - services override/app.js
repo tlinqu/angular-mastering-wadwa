969c58e608c155c2ce5b7a3937b5b7773c89190c
tlinqu <tlinqu@gmail.com>
2014-03-21 10:21:57 -0400

    Add comments regarding which service is picked when same named services defined in different modules

--- 1820OS_01_Code/16 - Modules and services - one big bag of services - services override/app.js
+++ 1820OS_01_Code/16 - Modules and services - one big bag of services - services override/app.js
@@ -1,3 +1,6 @@
+// If 'dieselEngine' is defined in root module i.e. app module, it will be used;
+// Here dieselEngine are defined in the two modules which have same distance with root module.
+// Since cars module is after engines module in ['engines', 'cars'], the dieselEngine in the cars module will be used.
 angular.module('app', ['engines', 'cars'])
   .controller('AppCtrl', function ($scope, car) {
     car.start();
