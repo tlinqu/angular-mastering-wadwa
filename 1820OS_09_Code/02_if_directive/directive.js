131aa6a724daafd63d8453044f87274543a94206
tlinqu <tlinqu@gmail.com>
2014-04-14 09:54:34 -0400

    Add comments for if directive regarding scope

--- 1820OS_09_Code/02_if_directive/directive.js
+++ 1820OS_09_Code/02_if_directive/directive.js
@@ -1,34 +1,35 @@
-angular.module('if-directive',[])
+angular.module('if-directive', [])
 
-.directive('if', [function () {
-  return {
-    transclude: 'element',
-    priority: 500,
-    compile: function (element, attr, transclude) {
-      return function (scope, element, attr) {
+    .directive('if', [function () {
+        return {
+            transclude: 'element',
+            priority: 500,
+            compile: function (element, attr, transclude) {
+                return function (scope, element, attr) {
 
-        var childElement;
-        var childScope;
- 
-        scope.$watch(attr['if'], function (newValue) {
-          
-          // Remove the element and scope as we are going to (re)create them now.
-          if (childElement) {
-            childElement.remove();
-            childElement = undefined;
-            childScope.$destroy();
-            childScope = undefined;
-          }
+                    var childElement;
+                    var childScope;
 
-          // If the attribute evaluates to true then we need to clone the transcluded DOM with a new scope
-          if (newValue) {
-            childScope = scope.$new();
-            childElement = transclude(childScope, function(clone) {
-              element.after(clone);
-            });
-          }
-        });
-      };
-    }
-  };
-}]);
+                    scope.$watch(attr['if'], function (newValue) {
+
+                        // Remove the element and scope as we are going to (re)create them now.
+                        if (childElement) {
+                            childElement.remove();
+                            childElement = undefined;
+                            childScope.$destroy();
+                            childScope = undefined;
+                        }
+
+                        // If the attribute evaluates to true then we need to clone the transcluded DOM with a new scope
+                        if (newValue) {
+                            // childScope = scope.$parent.$new(); // if scope:true or scope is an isolated scope
+                            childScope = scope.$new(); // scope:false
+                            childElement = transclude(childScope, function (clone) {
+                                element.after(clone);
+                            });
+                        }
+                    });
+                };
+            }
+        };
+    }]);
